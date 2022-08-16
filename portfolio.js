ff3ad987abf66414ff70859c9aedfb1c6b0d963c
Qamar Abbas <reply.qamar@gmail.com>
2022-08-16 14:28:27 +0500

    My Portfolio code

---
+++ portfolio.js
@@ -0,0 +1,205 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Qamar Abbas",
+  title: "Hi all, I'm Qamar",
+  description:
+    "Solution-oriented Back-end Software Engineer with an innate love of writing code to solve real-world problems Ability to manage time, coordinate with the team, and complete tasks on time. I am an excellent team player when it comes to team efforts to manipulate code. Can clearly communicate my ideas with the group, working with other team members to achieve the required goals. Always ready and excited to learn new things.",
+  resumeLink: "https://qamargondal.github.io/Resume/",
+};
+
+export const openSource = {
+  githubUserName: "qamargondal",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/qamar.abbas.9655/",
+  instagram: "https://www.instagram.com/qamarabbasx/",
+  twitter: "https://twitter.com/QamarAb83236772",
+  github: "https://github.com/qamargondal",
+  linkedin: "linkedin.com/in/qamar-abbas-8138b911a",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Integration of third party services such as Firebase/ AWS"),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "vscode-icons:file-type-sql",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "logos:docker-icon",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend/Design", //Insert stack or technology you have experience in
+    progressPercentage: "80", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "90",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "80",
+  },
+  {
+    Stack: "Database",
+    progressPercentage: "80",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "University Of Sargodha",
+    subHeader: "BS in Software Engineering",
+    duration: "September 2015 - June 2019",
+    desc: "In Final Year Project, Developed an application to aid visually impaired and blind people for object recognition and motion detection. Users will interact with the application via voice and the application will recognize the objects and the user will get the verbal response. ",
+    descBullets: ["Created Single Handed FYP", "Created Android Mobile App"],
+  },
+  // {
+  //   schoolName: "Punjab Group Of Colleges",
+  //   subHeader: "Intermediate in Computer Science",
+  //   duration: "April 2015 - September 2016",
+  //   desc: "ICS - Information Computer Science",
+  //   descBullets: ["Database Administrator", "C++ with Dev"],
+  // },
+  // {
+  //   schoolName: "Coursera",
+  //   subHeader: "Professional Certificates",
+  //   duration: "April 2020 - September 2021",
+  //   desc: "Certificates",
+  //   descBullets: [
+  //     "Server-side Development with NodeJS, Express and MongoDBDatbase Administrator",
+  //     "AWS Fundamentals: Going Cloud-Native",
+  //     "AWS Fundamentals: Building Serverless Applications",
+  //     "Version Control with Git",
+  //   ],
+  // },
+];
+
+export const experience = [
+  {
+    role: "Software Project Manager",
+    company: "Blinkers",
+    companylogo: "/img/icons/common/blinkers.jpg",
+    date: "January 2022 – Present",
+    desc: "Software Project Manager",
+    descBullets: [
+      "Technology Stack: NodeJs, JavaScript, Express, MySQL, PostgreSQL, MongoDB, Sequilize, Mongoose, Postman, GitHub.",
+      "Implementation of the Payment Gateways: Stripe and Checkout.",
+      "Effectively translated client requirements into application designs & system requirements.",
+      "Assisted in the collections and documentation of user requirements, development of user stories, and estimations.",
+    ],
+  },
+  {
+    role: "Frontend Developer",
+    company: "It Bouquet",
+    companylogo: "/img/icons/common/it_bouquet.png",
+    date: "September 2020 – September 2021",
+    desc: "Frontend Software Engineer",
+    descBullets: [
+      "Technology Stack: HTML, CSS, Bootstrap, JavaScript, Java, XML.",
+      "Worked as a productive and team member to design, code, test, report & debug operations.",
+      "Managed front-end & back-end development in company’s portfolio analyst.",
+      "Evaluated and became knowledgeable in new standards, technologies, and trends in website development.",
+    ],
+  },
+  //   {
+  //     role: "Software Engineer Intern",
+  //     company: "Airbnb",
+  //     companylogo: "/img/icons/common/airbnbLogo.png",
+  //     date: "Jan 2015 – Sep 2015",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   },
+];
+
+export const projects = [
+  {
+    name: "Blinkers Marketing",
+    desc: "Digital Marketing",
+    link: "https://blinkersmarketing.com/",
+  },
+  {
+    name: "Natours",
+    desc: "Natours is An awesome tour booking site built on top of NodeJS",
+    link: "https://natours-by-qamar.herokuapp.com/",
+  },
+  // {
+  //   name: "iGlows Admin Panel",
+  //   desc: "Admin Panel For iGlows Project in which admin can manage all the crud operations and handle the business logics of all Apps.",
+  //   link: "http://tac-admin-uat.s3-website-eu-west-1.amazonaws.com/auth/login",
+  // },
+  // {
+  //   name: "E-Commerce Admin Panel with Ebay Integeration",
+  //   desc: "Integerate the ebay with client application in which client can manage the orders in his Web Application",
+  //   link: "http://ebay.com",
+  // },
+];
+
+export const feedbacks = [
+  // {
+  //   name: "The Cars Hospital",
+  //   feedback:
+  //     "We have been very happy with our new web Project! It looks professional and very easy to navigate. Our experience with the customer service at Mohsin Ali has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose Mohsin web developer services for u.",
+  // },
+  {
+    name: "Blinkers Marketing",
+    feedback:
+      "We have been very happy with our new website! It looks professional and very easy to navigate.",
+  },
+];
+
+
+// "build": "SET NODE_OPTIONS=--openssl-legacy-provider && next build",
